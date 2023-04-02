cbe520defe939b5dbdb81eaacb18ac013c823bd0
Kenzy Hamed <104325633+Kenzyhamed@users.noreply.github.com>
2023-04-01 18:30:20 -0600

    Extra Info

--- CleaningCage.java
+++ CleaningCage.java
@@ -1,2 +1,8 @@
 public class CleaningCage {
+    /*
+    Cage cleaning is another activity not included in the database. It can be done at any time of day,
+and takes a consistent amount of time for each type of animal
+
+Each cage needs to be cleaned once per day
+     */
 }
--- FormatSchedule.java
+++ FormatSchedule.java
@@ -1,2 +1,10 @@
 public interface FormatSchedule {
+    /*
+     * Should create a file containing the schedule
+     *The schedule that you write must include the following information:
+o The hour that the task should be performed
+o The tasks to be performed in the hour
+o If the backup volunteer is require
+     */
+
 }
