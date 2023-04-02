3ab12fbfec1b2b0cca6cbefcfc3e2be9ee4fdf9d
Kenzy Hamed <104325633+Kenzyhamed@users.noreply.github.com>
2023-04-02 11:31:10 -0600

    Edits

--- Volunteers.java
+++ Volunteers.java
@@ -1,7 +1,5 @@
 public class Volunteers {
-    /*Sara needs to call the backup volunteer, the
-    program should inform her and it should not exit until she has confirmed that she has called the
-    volunteer for the necessary time(s).*/
+    /*Sara needs to call the backup volunteer, .*/
 
 
 }
