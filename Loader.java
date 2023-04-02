ea8aa763596c48247798d460671daf6e196a3e20
Kenzy Hamed <104325633+Kenzyhamed@users.noreply.github.com>
2023-04-01 19:00:57 -0600

    Loader edit

--- Loader.java
+++ Loader.java
@@ -2,6 +2,9 @@ public class Loader {
     /*
     this class loads  the sql database and puts each block of data into a hash map in the form of the <Integer, String[]>
     the integer part is the id and the rest are stored in the string list
-    this is where the program starts
+    this is where the program starts main()!
+
+    all hashmaps are public so we can use them along the classes and in the end it calls treatement()
+
      */
 }
